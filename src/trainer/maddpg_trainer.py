f959e9167b53ffa340820a583e8c6617389d1d0c
David Juanwu Lu <juanwu_lu@berkeley.edu>
2022-10-23 16:00:24 -0700

    [Oct 23] Update on the environment

--- src/trainer/maddpg_trainer.py
+++ src/trainer/maddpg_trainer.py
@@ -6,6 +6,7 @@
 """Trainer for Multi-agent Deep Deterministic Policy Gradient."""
 from typing import Any, Sequence
 
+from gym.core import Env
 from src.agent.base_agent import BaseAgent
 from src.memory.multi_agent_replay_buffer import MultiAgentReplayBuffer
 from src.trainer.base_trainer import Trainer
@@ -14,15 +15,19 @@ from src.trainer.base_trainer import Trainer
 class MADDPGTrainer(Trainer):
 
     def __init__(self,
+                 env: Env,
                  agents: Sequence[BaseAgent],
                  max_size: int = 10000) -> None:
         super().__init__()
 
+        self.env = env
         self.agents = agents
         self.replay_buffer = MultiAgentReplayBuffer(max_size=max_size)
 
     def train_one_epoch(self, epoch: int) -> Any:
         # TODO: Implement single epoch training
+
+        # Collect data for training
         pass
 
     def exec_one_epoch(self, epoch: int = -1) -> Any:
