a3882fe6feee616f9ed3bb5250221bf138ca55c5
Maverick Zhang <maverickzhang@berkeley.edu>
2022-10-23 17:15:38 -0700

    Revised init

--- src/environment/stock_market.py
+++ src/environment/stock_market.py
@@ -5,9 +5,11 @@
 # =============================================================================
 from __future__ import annotations
 
-from typing import Any, Tuple
+from typing import Any, Optional, Tuple, Union, Sequence
 
 import gym
+from gym.core import ActType, ObsType, Env
+from gym.spaces import Box, MultiDiscrete, Tuple as TupleSpace
 import numpy as np
 
 # TODO (Maverick): market maker agent (maybe not needed)
@@ -31,11 +33,52 @@ class StockMarketEnv(gym.Env):
         seed: random generator seed.
     """
 
-    def __init__(self, step_size: int = 1, seed: int = 0) -> None:
+    def __init__(self,
+                 num_agents: int,
+                 num_company: int = 5,
+                 num_correlated_stocks: int = 19,
+                 num_uncorrelated_stocks: int = 10,
+                 max_shares: int = 100000,
+                 start_prices: Union[float, Sequence[float]] = 100.0, # 1st value is target stock, next are correlated, final ones are uncorrelated
+                 min_budget: float = 100.0,
+                 max_budget: float = 10000.0,
+                 budget_discount: float = 0.9,
+                 step_size: float = 1.0,
+                 price_std: float = 100.0, # This may need to be tuned way smaller, possibly below 10
+                 noise_std: float = 10.0,
+                 seed: int = 0) -> None:
         super().__init__()
 
-        # add decorrelated stocks?
-        self.seed = seed
+        # Agent Parameters
+        self.num_agents = num_agents
+        self.num_company = num_company
+        self.min_budget = min_budget
+        self.max_budget = max_budget
+        self.budget_discount = budget_discount
+        self.max_shares = max_shares
+
+        # Stock Market Parameters
+        self.dt = step_size
+        self.start_prices = start_prices
+        self.price_std = price_std
+        self.noise_std = noise_std
+
+        # Observation and Action spaces
+        self.n_correlated_stocks = num_correlated_stocks
+        self.n_uncorrelated_stocks = num_uncorrelated_stocks
+        self.n_stocks = num_correlated_stocks + num_uncorrelated_stocks + 1
+        if not isinstance(start_prices, float):
+            assert self.n_stocks == self.start_prices.shape or self.n_stocks == self.start_prices.shape[0]
+        self.observation_space = Box(low=0.0,
+                                     high=float("inf"),
+                                     shape=(self.num_agents, self.n_stocks))
+        self.action_space = TupleSpace(
+            (Box(low=0.0, # Log Prices
+                 high=float("inf"),
+                 shape=(self.num_agents)),
+             MultiDiscrete([2 * max_shares + 1] * self.num_agents)) # Shares to put up
+        ) # 2 * max_shares + 1 for negative shares, and for 0 shares. Index 0 is 0, Index 1 - max_shares inclusive is negatives, Index max_shares + 1 - 2 * max_shares is positive
+        self._seed = seed
         self.reset()
 
         # np.random.seed(seed)
@@ -78,10 +121,27 @@ class StockMarketEnv(gym.Env):
         out.append(self.curr_state['company_states'].reshape(1, -1))
         return np.hstack(out).flatten()
 
-    def reset(self, seed=None):
+    def reset(self, 
+              seed: Optional[int] = None,
+              return_info: bool = True):
         if seed:
-            self.seed = seed
-        np.random.seed(seed=self.seed)
+            self._seed = seed
+        self.rng = np.random.default_rng(seed=seed or self._seed)
+
+        stocks = np.clip(
+            np.random.normal(loc=self.start_prices,
+                             scale=self.price_std,
+                             size=(self.n_stocks,)),
+            a_min=1., a_max=None # Can never have prices below 1 on stock market
+        )
+        target_stock = stocks[0]
+        correlated_stocks = stocks[1:self.n_correlated_stocks + 1]
+        uncorrelated_stocks = stocks[self.n_correlated_stocks + 1:]
+
+        self.eta = np.clip(
+            np.random.normal(loc=1.5, scale=1.5, size=(self.num_agents,)),
+            a_min=0, a_max=10
+        )
 
         def utility(c, eta):
             if eta != 1.:
@@ -101,16 +161,7 @@ class StockMarketEnv(gym.Env):
         self.ep_len = 390
         self.noise = 10.
         self.budget_discount = 0.9
-        correlated_stocks = np.clip(np.random.normal(
-            loc=self.start_price,
-            scale=self.std,
-            size=(self.num_correlated_stocks)
-        ), 1, None)
-        uncorrelated_stocks = np.clip(np.random.normal(
-            loc=self.start_price, scale=self.std, size=(self.num_uncorrelated_stocks)), 1, None)
-        total_stocks = 1 + self.num_correlated_stocks + self.num_uncorrelated_stocks
-        self.eta = np.clip(np.random.normal(
-            loc=1.5, scale=1.5, size=(self.num_agents)), 0, 10)
+        
         views = np.zeros((self.num_agents, total_stocks), dtype=np.int32)
         views[:, 0] = 1
         views[:, 1:1+self.num_correlated_stocks] = 1
